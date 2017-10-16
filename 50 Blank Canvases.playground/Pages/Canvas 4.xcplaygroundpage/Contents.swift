1a3dfb0835c1a5095db7b95840ffa8c711fa658e
Zhang Matt <mzhang@lcs.on.ca>
2017-10-16 11:21:49 -0400

    Made jawbreaker patterns.

--- 50 Blank Canvases.playground/Pages/Canvas 4.xcplaygroundpage/Contents.swift
+++ 50 Blank Canvases.playground/Pages/Canvas 4.xcplaygroundpage/Contents.swift
@@ -2,48 +2,33 @@
 //: # A Blank Canvas
 //:
 //: Use this page to experiment. Have fun!
-/*:
- ## Required code
- 
- The following statements are required to make the playground run.
- 
- Please do not remove.
- */
 import Cocoa
 import PlaygroundSupport
 
 // Create canvas
-let canvas = Canvas(width: 400, height: 300)
+let canvas = Canvas(width: 500, height: 500)
 
-/*:
- ## Add your code below
- 
- Be sure to write human-readable code.
- 
- Use whitespace and comments as appropriate.
- */
-// Replace this comment with your first comment – what is the goal of the code you're about to write?
+canvas.drawShapesWithFill = true
+canvas.drawShapesWithBorders = false
+
+//Colour generator
 
 
+for x in stride(from: 50, to: 500, by: 100)
+{
+    for y in stride(from: 50, to: 500, by: 100)
+    {
+        //# of rings per circle
+        for z in stride(from: 100, to: 0, by: -5)
+        {
+            let hue = random(from: 120, toButNotIncluding: 361)
+            canvas.fillColor = Color.init(hue: hue, saturation: 100, brightness: 100, alpha: 100)
+            canvas.drawEllipse(centreX: x, centreY: y, width: z, height: z)
+        }
+    }
+}
 
-/*:
- ## Use source control
- To keep your work organized, and receive feedback, source control is a must.
- 
- Please commit and push your work often.
- 
- ![source_control](source_control.png "Source Control")
- */
-/*:
- ## Template code
- The code below is necessary to see results in the Assistant Editor at right.
- 
- Please do not remove.
- 
- If you don't see output, remember to show the Assistant Editor, and switch to Live View:
- 
- ![timeline](timeline.png "Timeline")
- */
+
 // Don't remove the code below
 PlaygroundPage.current.liveView = canvas.imageView
 
