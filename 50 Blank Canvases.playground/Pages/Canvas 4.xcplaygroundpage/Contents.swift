3f4294402980bf23acdc5e71e8ab4a7390bc4833
Zhang Matt <mzhang@lcs.on.ca>
2017-10-16 22:48:26 -0400

    First attempt at no-repeat jawbreaker pattern.

--- 50 Blank Canvases.playground/Pages/Canvas 4.xcplaygroundpage/Contents.swift
+++ 50 Blank Canvases.playground/Pages/Canvas 4.xcplaygroundpage/Contents.swift
@@ -11,18 +11,37 @@ let canvas = Canvas(width: 500, height: 500)
 canvas.drawShapesWithFill = true
 canvas.drawShapesWithBorders = false
 
-//Colour generator
+//
+//let hue = random(from: 0, toButNotIncluding: 361)
+//if hue >= lasthue+20 && hue <= lasthue-20
+//{
+//    canvas.fillColor = Color(hue: hue, saturation: 100, brightness: 100, alpha: 100)
+//}
+
+
+//generate number
+//check number
+//if in range, regenerate number
+//set colour
+
+//generate number until out of range
+//set color
 
 
 for x in stride(from: 50, to: 500, by: 100)
 {
     for y in stride(from: 50, to: 500, by: 100)
     {
-        //# of rings per circle
-        for z in stride(from: 100, to: 0, by: -5)
+        for z in stride(from: 100, to: 0, by: -20)
         {
-            let hue = random(from: 120, toButNotIncluding: 361)
-            canvas.fillColor = Color.init(hue: hue, saturation: 100, brightness: 100, alpha: 100)
+            let hue = random(from: 0, toButNotIncluding: 361)
+            let largehue = hue+20
+            let smallhue = hue-20
+            if hue >= smallhue && hue <= largehue
+            {
+               let hue = random(from: 0, toButNotIncluding: 361)
+            }
+            canvas.fillColor = Color(hue: hue, saturation: 100, brightness: 100, alpha: 100)
             canvas.drawEllipse(centreX: x, centreY: y, width: z, height: z)
         }
     }
