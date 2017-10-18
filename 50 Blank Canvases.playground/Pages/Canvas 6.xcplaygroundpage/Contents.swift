2e0957518d2f6c5530718542c1cb44875a2d34cd
Zhang Matt <mzhang@lcs.on.ca>
2017-10-18 10:19:42 -0400

    I did what the teacher told me.

--- 50 Blank Canvases.playground/Pages/Canvas 6.xcplaygroundpage/Contents.swift
+++ 50 Blank Canvases.playground/Pages/Canvas 6.xcplaygroundpage/Contents.swift
@@ -13,37 +13,36 @@ import Cocoa
 import PlaygroundSupport
 
 // Create canvas
-let canvas = Canvas(width: 400, height: 300)
+let canvas = Canvas(width: 500, height: 500)
 
-/*:
- ## Add your code below
- 
- Be sure to write human-readable code.
- 
- Use whitespace and comments as appropriate.
- */
-// Replace this comment with your first comment – what is the goal of the code you're about to write?
+for i in stride(from: 0, through: canvas.height, by: 1)
+{
+    //Decide line height
+    let toY = random(from: 0, toButNotIncluding: canvas.height+1)
+    //Draw a vertical line
+    canvas.drawLine(fromX: i, fromY: 0, toX: i, toY: toY)
+}
 
+//Learn about data types
 
+var name = "Lakefield" //Assignment statement
+name = "Lakefield College School"
 
-/*:
- ## Use source control
- To keep your work organized, and receive feedback, source control is a must.
- 
- Please commit and push your work often.
- 
- ![source_control](source_control.png "Source Control")
- */
-/*:
- ## Template code
- The code below is necessary to see results in the Assistant Editor at right.
- 
- Please do not remove.
- 
- If you don't see output, remember to show the Assistant Editor, and switch to Live View:
- 
- ![timeline](timeline.png "Timeline")
- */
+//Create a number
+
+let value = 4 //Type interface guesses integer
+let anotherValue : Int = 5
+
+//Create a number that isnt an integer
+
+var decimalValue = 4.5
+decimalValue = 5
+let anotherDecimalValue : Double = 5.5
+
+Int.min
+
+//What are the maximum and mininum possible value
+
 // Don't remove the code below
 PlaygroundPage.current.liveView = canvas.imageView
 
